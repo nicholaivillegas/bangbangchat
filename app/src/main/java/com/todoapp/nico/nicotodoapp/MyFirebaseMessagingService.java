2a9cd6de23c57b99b1219c013675682e6768560c
Nico Villegas <john@tagcash.com>
2016-11-22 16:59:01 +0800

    Initial commit

---
+++ app/src/main/java/com/todoapp/nico/nicotodoapp/MyFirebaseMessagingService.java
@@ -0,0 +1,43 @@
+package com.todoapp.nico.nicotodoapp;
+
+import android.app.NotificationManager;
+import android.content.Context;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+/**
+ * Created by Nico on 11/22/2016.
+ */
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    private static final String TAG = "FCM Service";
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // TODO: Handle FCM messages here.
+        // If the application is in the foreground handle both data and notification messages here.
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated.
+
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+        Log.d(TAG, "Notification Message Body: " + remoteMessage.getNotification().getBody());
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this);
+        mBuilder.setSmallIcon(R.drawable.common_ic_googleplayservices);
+        mBuilder.setContentTitle(remoteMessage.getNotification().getTitle());
+        mBuilder.setContentText(remoteMessage.getNotification().getBody());
+        NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        Uri alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        mBuilder.setSound(alarmSound);
+        long[] pattern = {500,500,500,500,500,500,500,500,500};
+        mBuilder.setVibrate(pattern);
+
+        // notificationID allows you to update the notification later on.
+        mNotificationManager.notify(remoteMessage.getTtl(), mBuilder.build());
+    }
+}
